8d22d21031380c42d1f5fb8c5e47fc43d0f9503b
Brage Vik <brage.vik@lyse.net>
2025-12-01 10:45:32 +0100

    feat: complete day 1

--- src/day01.rs
+++ src/day01.rs
@@ -1,7 +1,42 @@
-pub fn task1(_input: &str) -> String {
-    todo!("Day 1 task 1")
+pub fn task1(input: &str) -> String {
+    let mut dial: i32 = 50;
+    let mut zero_count = 0;
+    for line in input.lines() {
+        let direction = line.bytes().next().unwrap();
+        let distance: i32 = line[1..].parse().unwrap();
+        match direction {
+            b'L' => dial = (dial - distance).rem_euclid(100),
+            b'R' => dial = (dial + distance).rem_euclid(100),
+            _ => panic!(),
+        }
+        if dial == 0 {
+            zero_count += 1;
+        }
+    }
+    zero_count.to_string()
 }
 
-pub fn task2(_input: &str) -> String {
-    todo!("Day 1 task 2")
+pub fn task2(input: &str) -> String {
+    let mut dial: i32 = 50;
+    let mut zero_count = 0;
+    for line in input.lines() {
+        let direction = line.bytes().next().unwrap();
+        let distance: i32 = line[1..].parse().unwrap();
+        let was_zero = dial == 0;
+        match direction {
+            b'L' => dial -= distance,
+            b'R' => dial += distance,
+            _ => panic!(),
+        }
+        if dial <= 0 {
+            zero_count += -dial / 100 + 1;
+            if was_zero {
+                zero_count -= 1;
+            }
+        } else if dial > 99 {
+            zero_count += dial / 100;
+        }
+        dial = dial.rem_euclid(100);
+    }
+    zero_count.to_string()
 }
--- src/test.rs
+++ src/test.rs
@@ -6,3 +6,39 @@ macro_rules! assert_task {
         assert_eq!(solution, $expected.to_string());
     }};
 }
+
+#[test]
+fn test_day1_task1() {
+    assert_task!(
+        day01::task1,
+        "L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82",
+        "3"
+    )
+}
+
+#[test]
+fn test_day1_task2() {
+    assert_task!(
+        day01::task2,
+        "L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82",
+        "6"
+    )
+}
