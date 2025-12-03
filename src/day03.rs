e65724a1ad9177a540a8e3f09ab8834c9a91e436
Brage Vik <brage.vik@lyse.net>
2025-12-03 10:33:51 +0100

    feat: complete day 3 task 1

--- src/day03.rs
+++ src/day03.rs
@@ -1,5 +1,28 @@
-pub fn task1(_input: &str) -> String {
-    todo!("Day 3 task 1")
+fn max_joltage(line: &str) -> u32 {
+    let bytes = line.as_bytes();
+    let mut first = bytes[0];
+    let mut first_i = 0;
+    for (i, c) in bytes[..(bytes.len() - 1)].iter().enumerate().skip(1) {
+        if *c > first {
+            first = *c;
+            first_i = i;
+        }
+    }
+    let mut second = bytes[first_i + 1];
+    for c in bytes[(first_i + 2)..].iter() {
+        if *c > second {
+            second = *c;
+        }
+    }
+    ((first - b'0') * 10 + second - b'0') as u32
+}
+
+pub fn task1(input: &str) -> String {
+    input
+        .lines()
+        .map(max_joltage)
+        .sum::<u32>()
+        .to_string()
 }
 
 pub fn task2(_input: &str) -> String {
