7edec174b05016206b9fcb23e9a3158e0517a55c
Brage Vik <brage.vik@lyse.net>
2025-12-03 13:43:18 +0100

    feat: complete day 3 task 2

--- src/day03.rs
+++ src/day03.rs
@@ -1,30 +1,36 @@
-fn max_joltage(line: &str) -> u32 {
-    let bytes = line.as_bytes();
-    let mut first = bytes[0];
-    let mut first_i = 0;
-    for (i, c) in bytes[..(bytes.len() - 1)].iter().enumerate().skip(1) {
-        if *c > first {
-            first = *c;
-            first_i = i;
+fn max_subseqence(digits: &str, digit_count: u32) -> u64 {
+    let digits = digits.as_bytes();
+    let mut total = 0;
+    let mut progress = 0;
+    for digit in (0..digit_count).rev() {
+        let mut max = 0;
+        for (i, digit) in digits[..(digits.len() - digit as usize)]
+            .iter()
+            .enumerate()
+            .skip(progress)
+        {
+            if *digit > max {
+                max = *digit;
+                progress = i + 1;
+            }
         }
+        total += 10_u64.pow(digit) * u64::from(max - b'0');
     }
-    let mut second = bytes[first_i + 1];
-    for c in bytes[(first_i + 2)..].iter() {
-        if *c > second {
-            second = *c;
-        }
-    }
-    ((first - b'0') * 10 + second - b'0') as u32
+    total
 }
 
-pub fn task1(input: &str) -> String {
+fn solve_tasks(input: &str, battery_count: u32) -> String {
     input
         .lines()
-        .map(max_joltage)
-        .sum::<u32>()
+        .map(|digits| max_subseqence(digits, battery_count))
+        .sum::<u64>()
         .to_string()
 }
 
-pub fn task2(_input: &str) -> String {
-    todo!("Day 3 task 2")
+pub fn task1(input: &str) -> String {
+    solve_tasks(input, 2)
+}
+
+pub fn task2(input: &str) -> String {
+    solve_tasks(input, 12)
 }
