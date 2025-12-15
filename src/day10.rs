d4ca3a220393df0696f18235f20a86fdb1813c2a
Brage Vik <brage.vik@lyse.net>
2025-12-15 21:20:26 +0100

    feat: start day 10

--- src/day10.rs
+++ src/day10.rs
@@ -1,4 +1,35 @@
-pub fn task1(_input: &str) -> String {
+use regex::Regex;
+
+struct Machine {
+    indicator_lights: Vec<bool>,
+    buttons: Vec<Vec<usize>>,
+    _joltage: Vec<u8>,
+}
+
+fn parse_machines(input: &str) -> Vec<Machine> {
+    let machine_re = Regex::new(
+        r"(?x)
+    \[(?<lights>[\.\#]+)\]\ 
+    (?<buttons>(\((\d+,?)*\)\ )*)
+    \{(?<joltage>(\d+,?)*)\}
+    ",
+    )
+    .unwrap();
+    let re = Regex::new(r"\((\d*)(,)?\)").unwrap();
+    let a: Vec<()> = input
+        .lines()
+        .map(|line| {
+            let caps = machine_re.captures(line).unwrap();
+            println!("{caps:?}");
+            // let (_, buttons) = re.captures(line).unwrap().extract();
+            // println!("{buttons:?}");
+        })
+        .collect();
+    todo!()
+}
+
+pub fn task1(input: &str) -> String {
+    parse_machines(input);
     todo!("Day 10 task 1")
 }
 
