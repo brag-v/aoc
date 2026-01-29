f8a643d213b29a9ddc31fa6db49a0f3f1f7f7405
Brage Vik <brage.vik@lyse.net>
2026-01-29 14:47:43 +0100

    chore: improve code style in day 9

--- src/day09.rs
+++ src/day09.rs
@@ -1,9 +1,9 @@
 use std::collections::VecDeque;
 
-fn is_sum_of_pair(num: &u64, previous_nums: &VecDeque<u64>) -> bool {
+fn is_sum_of_pair(num: u64, previous_nums: &VecDeque<u64>) -> bool {
     for i in 0..previous_nums.len() {
         for j in (i + 1)..previous_nums.len() {
-            if previous_nums[i] + previous_nums[j] == *num {
+            if previous_nums[i] + previous_nums[j] == num {
                 return true;
             }
         }
@@ -11,46 +11,54 @@ fn is_sum_of_pair(num: &u64, previous_nums: &VecDeque<u64>) -> bool {
     false
 }
 
-fn first_invalid_number(input: &str, preamble_length: usize) -> u64 {
-    let mut nums = input.lines().map(|num| num.parse().unwrap());
-    let mut preamble: VecDeque<u64> = nums.by_ref().take(preamble_length).collect();
-    nums.find(|num| {
-        if !is_sum_of_pair(num, &preamble) {
-            true
-        } else {
-            preamble.pop_front();
-            preamble.push_back(*num);
-            false
-        }
-    })
-    .unwrap()
+fn first_invalid_number(nums: &[u64], preamble_length: usize) -> u64 {
+    let mut previous_nums = VecDeque::with_capacity(preamble_length);
+    previous_nums.extend(nums[..preamble_length].iter());
+    *nums[preamble_length..]
+        .iter()
+        .find(|num| {
+            if is_sum_of_pair(**num, &previous_nums) {
+                previous_nums.pop_front();
+                previous_nums.push_back(**num);
+                false
+            } else {
+                true
+            }
+        })
+        .unwrap()
 }
 
 pub fn task1_custom_preamble(input: &str, preamble_length: usize) -> String {
-    first_invalid_number(input, preamble_length).to_string()
+    let nums: Box<[u64]> = input.lines().map(|num| num.parse().unwrap()).collect();
+    first_invalid_number(&nums, preamble_length).to_string()
 }
 
 pub fn task1(input: &str) -> String {
     task1_custom_preamble(input, 25)
 }
 
-pub fn task2_custom_preamble(input: &str, preamble_length: usize) -> String {
-    let num = first_invalid_number(input, preamble_length);
-    let sequence: Box<[u64]> = input.lines().map(|num| num.parse().unwrap()).collect();
-    for i in 0..sequence.len() {
-        let mut acc = sequence[i];
-        for j in (i + 1)..sequence.len() {
-            acc += sequence[j];
-            if acc > num {
+fn find_contigous_sum(nums: &[u64], target_sum: u64) -> Option<&[u64]> {
+    for i in 0..nums.len() {
+        let mut acc = nums[i];
+        for j in (i + 1)..nums.len() {
+            acc += nums[j];
+            if acc > target_sum {
                 break;
-            } else if acc == num {
-                return (sequence[i..=j].iter().min().unwrap()
-                    + sequence[i..=j].iter().max().unwrap())
-                .to_string();
+            } else if acc == target_sum {
+                return Some(&nums[i..=j]);
             }
         }
     }
-    panic!()
+    None
+}
+
+pub fn task2_custom_preamble(input: &str, preamble_length: usize) -> String {
+    let nums: Box<[u64]> = input.lines().map(|num| num.parse().unwrap()).collect();
+    let invalid_num = first_invalid_number(&nums, preamble_length);
+    match find_contigous_sum(&nums, invalid_num) {
+        Some(range) => (range.iter().min().unwrap() + range.iter().max().unwrap()).to_string(),
+        None => panic!(),
+    }
 }
 
 pub fn task2(input: &str) -> String {
