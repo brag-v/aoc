7c849f2f30db57a6ad2fe6c9b1dd11f8094b7bfb
Brage Vik <brage.vik@lyse.net>
2026-01-30 16:02:32 +0100

    feat: complete day 10 task 2

--- src/day10.rs
+++ src/day10.rs
@@ -1,8 +1,13 @@
-pub fn task1(input: &str) -> String {
+fn parse_adapters(input: &str) -> Vec<u32> {
     let mut nums: Vec<u32> = input.lines().map(|num| num.parse().unwrap()).collect();
     nums.push(0);
     nums.sort_unstable();
     nums.push(*nums.last().unwrap() + 3);
+    nums
+}
+
+pub fn task1(input: &str) -> String {
+    let nums = parse_adapters(input);
     let (ones, threes) =
         nums.windows(2)
             .map(|pair| pair[1] - pair[0])
@@ -18,6 +23,23 @@ pub fn task1(input: &str) -> String {
     (ones * threes).to_string()
 }
 
-pub fn task2(_input: &str) -> String {
-    todo!("Day 10 task 2")
+pub fn task2(input: &str) -> String {
+    let adapters = parse_adapters(input);
+    let n = adapters.len();
+    let mut possible_arrangements: Vec<u64> = vec![0; n];
+    // base case: last adapter (output) has 1 trivial arrangment (itself)
+    *possible_arrangements.last_mut().unwrap() = 1;
+    // buttom up calculation:
+    for i in (0..(n - 1)).rev() {
+        // possible arrangment from adapter i is the sum of all 
+        // possible arrangments reachable from i (within 3 joltage levels)
+        for j in (i + 1)..n {
+            if adapters[j] > adapters[i] + 3 {
+                break;
+            }
+            possible_arrangements[i] += possible_arrangements[j]
+        }
+    }
+    // return possible arrangmetns from first adapter (input)
+    possible_arrangements[0].to_string()
 }
