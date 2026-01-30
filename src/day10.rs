a3482ec2ff03ad4f2c89d8e7d261eea1d94f1380
Brage Vik <brage.vik@lyse.net>
2026-01-30 15:10:58 +0100

    feat: complete day 10 task 1

--- src/day10.rs
+++ src/day10.rs
@@ -1,5 +1,21 @@
-pub fn task1(_input: &str) -> String {
-    todo!("Day 10 task 1")
+pub fn task1(input: &str) -> String {
+    let mut nums: Vec<u32> = input.lines().map(|num| num.parse().unwrap()).collect();
+    nums.push(0);
+    nums.sort_unstable();
+    nums.push(*nums.last().unwrap() + 3);
+    let (ones, threes) =
+        nums.windows(2)
+            .map(|pair| pair[1] - pair[0])
+            .fold((0, 0), |(ones, threes), difference| {
+                if difference == 1 {
+                    (ones + 1, threes)
+                } else if difference == 3 {
+                    (ones, threes + 1)
+                } else {
+                    (ones, threes)
+                }
+            });
+    (ones * threes).to_string()
 }
 
 pub fn task2(_input: &str) -> String {
