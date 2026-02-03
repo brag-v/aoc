0fc8dbf14b65d5eafeaf54aec981dc8a0c332210
Brage Vik <brage.vik@lyse.net>
2026-02-03 22:29:37 +0100

    build: add files for days 13 through 25

---
+++ src/day25.rs
@@ -0,0 +1,7 @@
+pub fn task1(_input: &str) -> String {
+    todo!("Day 1 task 1")
+}
+
+pub fn task2(_input: &str) -> String {
+    todo!("Day 1 task 2")
+}
--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,19 @@ mod day09;
 mod day10;
 mod day11;
 mod day12;
+mod day13;
+mod day14;
+mod day15;
+mod day16;
+mod day17;
+mod day18;
+mod day19;
+mod day20;
+mod day21;
+mod day22;
+mod day23;
+mod day24;
+mod day25;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
