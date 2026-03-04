c40e8c4caeeea62e8c8ee78890631b666ed5c709
Brage Vik <brage.vik@lyse.net>
2026-03-04 16:00:50 +0100

    feat: complete day 19 task 2

--- src/day19.rs
+++ src/day19.rs
@@ -1,74 +1,105 @@
+use rayon::{iter::ParallelIterator, str::ParallelString};
+
 #[derive(Debug, Clone)]
 enum Rule {
     Recursive(Vec<Vec<usize>>),
     Letter(char),
 }
 
-fn parse_rules(rule_input: &str) -> Vec<Rule> {
-    let mut stated_rules = vec![None; rule_input.lines().count()];
+fn parse_rule(rule: &str) -> Rule {
+    if rule.starts_with('"') {
+        Rule::Letter(rule.chars().nth(1).unwrap())
+    } else {
+        Rule::Recursive(
+            rule.split(" | ")
+                .map(|sequence| {
+                    sequence
+                        .split(' ')
+                        .map(|index| index.parse().unwrap())
+                        .collect()
+                })
+                .collect(),
+        )
+    }
+}
+
+fn parse_rules(rule_input: &str) -> Vec<Option<Rule>> {
+    let mut stated_rules = vec![None; rule_input.lines().count().max(43)];
     for line in rule_input.lines() {
         let (i, rule) = line.split_once(": ").unwrap();
-        let rule = if rule.starts_with('"') {
-            Rule::Letter(rule.chars().nth(1).unwrap())
-        } else {
-            Rule::Recursive(
-                rule.split(" | ")
-                    .map(|sequence| {
-                        sequence
-                            .split(' ')
-                            .map(|index| index.parse().unwrap())
-                            .collect()
-                    })
-                    .collect(),
-            )
-        };
-        stated_rules[i.parse::<usize>().unwrap()] = Some(rule);
+        stated_rules[i.parse::<usize>().unwrap()] = Some(parse_rule(rule));
     }
 
     stated_rules
-        .iter()
-        .map(|rule| rule.to_owned().unwrap())
-        .collect()
 }
 
-fn is_valid(message: &str, rules: &[Rule], base_rule: usize) -> Option<usize> {
-    match &rules[base_rule] {
-        Rule::Recursive(choices) => {
-            'choice: for sequence in choices {
-                let mut prefix_length = 0;
-                for sub_rule in sequence {
-                    match is_valid(&message[prefix_length..], rules, *sub_rule) {
-                        Some(l) => prefix_length += l,
-                        None => continue 'choice,
-                    }
-                }
-                return Some(prefix_length);
-            }
-            None
-        }
-        Rule::Letter(c) => {
-            if *c == message.chars().next().unwrap() {
-                Some(1)
-            } else {
-                None
+fn valid_prefixes_rule_sequence(
+    message: &str,
+    rules: &[Option<Rule>],
+    rule_sequence: &[usize],
+) -> Box<[usize]> {
+    let mut prefix_lengths = vec![0];
+    for sub_rule in rule_sequence {
+        let mut new_prefix_lengths = vec![];
+        for prefix_length in prefix_lengths {
+            for added_length in valid_prefixes(&message[prefix_length..], rules, *sub_rule) {
+                new_prefix_lengths.push(prefix_length + added_length);
             }
         }
+        prefix_lengths = new_prefix_lengths;
+        prefix_lengths.sort_unstable();
+        prefix_lengths.dedup();
     }
+    prefix_lengths.into_boxed_slice()
+}
+
+fn valid_prefixes(message: &str, rules: &[Option<Rule>], base_rule: usize) -> Box<[usize]> {
+    match &rules[base_rule] {
+        Some(Rule::Recursive(choices)) => {
+            let mut prefix_lengths = vec![];
+            for sequence in choices {
+                prefix_lengths
+                    .extend(valid_prefixes_rule_sequence(message, rules, sequence).iter());
+            }
+            prefix_lengths.sort_unstable();
+            prefix_lengths.dedup();
+            prefix_lengths.into_boxed_slice()
+        }
+        Some(Rule::Letter(rule_c)) => {
+            if message
+                .chars()
+                .next()
+                .is_some_and(|message_c| message_c == *rule_c)
+            {
+                Box::new([1])
+            } else {
+                Box::new([])
+            }
+        }
+        None => panic!("Encoundered reference to uninitialized rule: {base_rule}"),
+    }
+}
+
+fn valid_message_count(messages: &str, rules: &[Option<Rule>]) -> usize {
+    messages
+        .par_lines()
+        .filter(|message| valid_prefixes(message, rules, 0).contains(&message.len()))
+        .count()
 }
 
 pub fn task1(input: &str) -> String {
     let (rule_input, messages) = input.split_once("\n\n").unwrap();
     let rules = parse_rules(rule_input);
 
-    messages
-        .lines()
-        .filter(|message| {
-            is_valid(message, &rules, 0).is_some_and(|length| length == message.len())
-        })
-        .count()
-        .to_string()
+    valid_message_count(messages, &rules).to_string()
 }
 
-pub fn task2(_input: &str) -> String {
-    todo!("Day 1 task 2")
+pub fn task2(input: &str) -> String {
+    let (rule_input, messages) = input.split_once("\n\n").unwrap();
+    let mut rules = parse_rules(rule_input);
+
+    rules[8] = Some(parse_rule("42 | 42 8"));
+    rules[11] = Some(parse_rule("42 31 | 42 11 31"));
+
+    valid_message_count(messages, &rules).to_string()
 }
